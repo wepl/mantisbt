b4057d05b6ec96d2aa29e0bba5bc59ac9d7acd1d
unknown <Paul@.(none)>
2011-07-28 20:27:17 +0100

    pass in default value if not specified

--- core/classes/MantisDatabaseException.class.php
+++ core/classes/MantisDatabaseException.class.php
@@ -1,7 +1,7 @@
 <?php
 
 class MantisDatabaseException extends MantisException {
-    public function __construct($code = 0, $parameters, Exception $previous = null)
+    public function __construct($code = 0, $parameters = null, Exception $previous = null)
     {
 		/* if we have some form of database exception, assume that the database don't want to treat
 		 * the database as connected in the exception handler anymore
